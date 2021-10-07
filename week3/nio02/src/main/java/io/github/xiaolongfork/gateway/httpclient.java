fce6bfdf88ded28ebdcdfd4c730cbe039726052b
jiangzuofei <jiangzuofei@126.com>
2021-10-07 16:43:04 +0800

    1.（必做）整合你上次作业的 httpclient/okhttp； 3.（必做）实现过滤器。

---
+++ week3/nio02/src/main/java/io/github/xiaolongfork/gateway/httpclient.java
@@ -0,0 +1,33 @@
+package io.github.xiaolongfork.gateway;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class httpclient {
+    private OkHttpClient client = new OkHttpClient();
+
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://localhost:8888/test";
+        try {
+            Request request = new Request.Builder().url(url).build();
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                System.out.println(response.body().string());
+            } else {
+                System.out.println("连接失败");
+            }
+        } catch (IOException e) {
+            StringWriter sw=new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            System.out.println(sw.getBuffer().toString());
+        }
+    }
+
+}
+
